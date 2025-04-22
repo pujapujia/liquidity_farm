2b576584f6dcfea9f6cfc2a0b3bf06d1b38736eb
pujapujia <pujira02@gmail.com>
2025-04-23 07:42:47 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+// Ganti dengan Firebase Config kamu dari Firebase Console
+const firebaseConfig = {
+    apiKey: "AIzaSyDUGk4PBFp2WS6gsMCkFcCUMmEQIWL-cYs",
+    authDomain: "webfarm-bdc35.firebaseapp.com",
+    projectId: "webfarm-bdc35",
+    storageBucket: "webfarm-bdc35.firebasestorage.app",
+    messagingSenderId: "171163595158",
+    appId: "1:171163595158:web:52b28d744349f36cd06db7",
+    measurementId: "G-3VG524T04J"
+  }
+  
+  // Inisialisasi Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
